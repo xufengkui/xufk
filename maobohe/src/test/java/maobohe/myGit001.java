4a1732ba28f743f2f145bf23a7f0b187fdae135c
xufk <>
2019-01-10 17:16:17 +0800

    323242

--- maobohe/src/test/java/maobohe/myGit001.java
+++ maobohe/src/test/java/maobohe/myGit001.java
@@ -4,5 +4,6 @@ public class myGit001 {
     public static void main(String[] args) {
         System.out.println("我是001");
         System.out.println("我还是0052522");
+        System.out.println("121");
     }
 }
